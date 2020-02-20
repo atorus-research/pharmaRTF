ca4a3be833d90b0aeb10b137cbb28db273c7bb6c
mstackhouse <mike.stackhouse@atorusresearch.com>
2020-02-20 22:00:40 +0000

    started exploring gt

--- scripts/gt_table.R
+++ scripts/gt_table.R
@@ -1,43 +1,30 @@
 ## Make the table in GT ----
 
-source('./assemble_data.R')
-
-footnote <- glue("NOTE: Duration of disease is computed as months between date of enrollment and date \\
-                of onset of the first definite symptoms of Alzheimer's disease.")
+source('./scripts/assemble_data.R')
+library(gt)
 
 t <- final %>%
+  select(rowlbl1, rowlbl2, Pbo, Xan_Lo, Xan_Hi, Tot) %>%
   # Create the table object
-  gt(rowname_col = "rowlbl2", groupname_col = "rowlbl1") %>%
-  # Titles
-  tab_header(
-    title = "14-2.01",
-    subtitle = "Summary of Demographic and Baseline Characteristics"
-  ) %>%
-  # Footnotes
-  tab_source_note(
-    source_note = footnote
-  ) %>%
+  gt() %>%
   # Adjust labels
   cols_label(
+    rowlbl1 = '',
+    rowlbl2 = '',
     Pbo = md(header_n_v$Pbo),
     Xan_Hi = md(header_n_v$Xan_Hi),
     Xan_Lo = md(header_n_v$Xan_Lo),
     Tot = md(header_n_v$Tot),
-  ) %>%
-  cols_move(
-    columns = c('Pbo', 'Xan_Lo', 'Xan_Hi', 'Tot'),
-    after = 'rowlbl2'
   ) %>%
   # Center labels
   tab_style(
     style=cell_text(align='center'),
     locations = cells_column_labels(everything()) # this formats incorrectly but noted in bug 439
   ) %>%
-  # Bold the groups
-  tab_style(
-    style=cell_text(weight='bold'),
-    locations = cells_row_groups(everything())
-  )
+  as_rtf()
 
 t %>%
   gtsave('./test.html')
+
+## TODO: Dig into the GT package to and make an edit of the as_rtf function. Steal the components but write just the header
+##       to the titles and write the body to the body. gt::as_rtf() is located in export.R.
