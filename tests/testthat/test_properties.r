45166bf79537a38e69043aa6af9981b7425841b9
Eli Miller <emiller@AR-RStudioDev.4ygzttwrp44enh1pozggg5gc4g.cx.internal.cloudapp.net>
2020-03-20 20:42:45 +0000

    Update tests for properties

--- tests/testthat/test_properties.r
+++ tests/testthat/test_properties.r
@@ -1,23 +1,22 @@
 ## TODO:
 # Add tests for gt set and get
-
-
-
 context("properties")
 library(huxtable)
-library(pharmaRTF)
+
 # library(gt)
 
+ht_tab <- hux(
+  column1 = 1:5,
+  column2 = letters[1:5]
+)
+
+
 test_that("Font getters property return all font(s)", {
-  ht <- huxtable(
-    column1 = 1:5,
-    column2 = letters[1:5]
-  )
-  huxtable::font(ht)[1,1] <- "Times New Roman"
-  huxtable::font(ht)[2,2] <- "Comic Sans"
-  huxtable::font(ht)[4,2] <- "Windings"
 
 
-  expect_true(TRUE)
+
+  expect_true(all(font(ht_tab) %in% c("Times New Roman", NA, "Comic Sans", "Windings")) &
+              all(c("Times New Roman", NA, "Comic Sans", "Windings") %in% font(ht_tab)) &
+              length(font(ht_tab) == 4))
 })
 
