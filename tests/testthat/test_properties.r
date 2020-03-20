8337a6e68c3157f3b42a1ac02bbebb2258ca4ec3
Eli Miller <emiller@AR-RStudioDev.4ygzttwrp44enh1pozggg5gc4g.cx.internal.cloudapp.net>
2020-03-20 20:00:28 +0000

    Add tests for fonts

--- tests/testthat/test_properties.r
+++ tests/testthat/test_properties.r
@@ -1,5 +1,23 @@
-context("properties")
+## TODO:
+# Add tests for gt set and get
 
-test_that("placeholder", {
+
+
+context("properties")
+library(huxtable)
+library(pharmaRTF)
+# library(gt)
+
+test_that("Font getters property return all font(s)", {
+  ht <- huxtable(
+    column1 = 1:5,
+    column2 = letters[1:5]
+  )
+  huxtable::font(ht)[1,1] <- "Times New Roman"
+  huxtable::font(ht)[2,2] <- "Comic Sans"
+  huxtable::font(ht)[4,2] <- "Windings"
+
+
   expect_true(TRUE)
 })
+
