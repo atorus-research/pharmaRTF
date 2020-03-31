b5be0185f26792c76fc3d7297df4f65fb3a41d7a
Eli Miller <emiller@AR-RStudioDev.4ygzttwrp44enh1pozggg5gc4g.cx.internal.cloudapp.net>
2020-03-31 13:39:34 +0000

    Update tests

--- tests/testthat/test_rtf-code-generators.r
+++ tests/testthat/test_rtf-code-generators.r
@@ -102,7 +102,7 @@ test_that("header_string lines populates correctly" ,{
   #                      end = str_length(read_file("headers2.txt"))-1))
 })
 
-test_that("write_rtf writes an expected rtf_file", {
+test_that("write_rtf writes an expected rtf_file - 1", {
   mat <- matrix(
     c(1:5, letters[1:5], LETTERS[1:5],
     6:10, letters[6:10],LETTERS[6:10]),
@@ -113,6 +113,7 @@ test_that("write_rtf writes an expected rtf_file", {
   ht <- huxtable(
     mat
   )
+  ht <- huxtable::set_font(ht, 1, 1:5, "Times")
   huxtable::font_size(ht)[,2] <- 15
   huxtable::bold(ht)[,3] <- TRUE
   huxtable::italic(ht)[,4] <- TRUE
@@ -159,6 +160,43 @@ test_that("write_rtf writes an expected rtf_file", {
 
 })
 
+test_that("write_rtf writes an expected rtf_file - 2", {
+  ht <- huxtable(
+    col1 <- letters[1:25],
+    col2 <- LETTERS[1:25],
+    col3 <- 1:25
+  )
+  titles <- list(
+    hf_line("A Test RTF Document 2"),
+    hf_line("Manually checked and used for changes to the package", align = "left")
+  )
+  footers <- list(
+    hf_line("These are footers"),
+    hf_line(c("They can have page numbers", "PAGE_FORMAT: Page %s of %s"), align = "split"),
+    hf_line(c("Or a different format", "PAGE_FORMAT: Page %s  Total Pages %s"), align = "split", bold = TRUE,
+            font_size = 10, italic = TRUE),
+    hf_line(c("Here is a date format at the top", "DATE_FORMAT: %H:%M %A, %B %d, %Y"), index = 1),
+    hf_line("FILE_PATH: File source: %s", index = 2)
+  )
 
+  rtf <- rtf_doc(ht, header.rows = 3, titles = titles)
+  #This statement is blocked by issue 85
+  rtf <- add_footnotes(rtf, hf_line("These are footers"),
+                       hf_line(c("They can have page numbers", "PAGE_FORMAT: Page %s of %s"), align = "split"),
+                       hf_line(c("Or a different format", "PAGE_FORMAT: Page %s  Total Pages %s"), align = "split", bold = TRUE,
+                               font_size = 10, italic = TRUE),
+                       hf_line(c("Here is a date format at the top", "DATE_FORMAT: %H:%M %A, %B %d, %Y"), index = 1),
+                       hf_line("FILE_PATH: File source: %s", index = 2))
+
+  margins(rtf) <- c(top = 0.5, bottom = 0.5, left = 0.5, right = 0.5)
+  orientation(rtf) <- "landscape"
+  header_height(rtf) <- 1.5
+  footer_height(rtf) <- 2
+  pagesize(rtf) <- c(height = 8, width = 8)
+
+  # write_rtf(rtf)
+  expect_true(TRUE)
+
+})
 
 
