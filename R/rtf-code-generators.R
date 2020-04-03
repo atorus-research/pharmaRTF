e2dcbd3ef06c4ba8de6c0a10e96013a507315750
mstackhouse <mike.stackhouse@atorusresearch.com>
2020-04-03 21:38:34 +0000

    added font size checks to close #100

--- R/rtf-code-generators.R
+++ R/rtf-code-generators.R
@@ -136,7 +136,7 @@ hf_line_string <- function(line, doc=NULL) {
 
   # If font size is overridden generate the string
   if (!is.na(font_size(line))) {
-    fs <- sprintf("\\fs%s", font_size(line))
+    fs <- sprintf("\\fs%s", font_size(line)*2)
   }
 
   # Styling
