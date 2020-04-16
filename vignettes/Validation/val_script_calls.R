9ddd5efe14e620dbcf0e8c218267e6afb6a14237
Eli Miller <emiller@AR-RStudioDev.4ygzttwrp44enh1pozggg5gc4g.cx.internal.cloudapp.net>
2020-04-16 18:27:36 +0000

    Update val script

--- vignettes/Validation/val_script_calls.R
+++ vignettes/Validation/val_script_calls.R
@@ -1,10 +1,7 @@
 #Validation Script
 
 # Source all of the files in the rtf_test_files directory
-lapply(list.files("vignettes/Validation/Test_Case_Code/rtf_test_files", full.names = TRUE,
+lapply(list.files("vignettes/Validation/Test_Case_Code", full.names = TRUE,
                   pattern = ".R$"), source)
-
-# Currently done manually
-# source("vignettes/Validation/write_vur.R")
 
 shiny::runApp("vignettes/Validation/app.R")
