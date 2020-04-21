86f6ffbbfd5507c7dacff54a5256b74f7af26595
Eli Miller <emiller@AR-RStudioDev.4ygzttwrp44enh1pozggg5gc4g.cx.internal.cloudapp.net>
2020-04-21 13:47:39 +0000

    update issue 39 table 14-6.04

--- scripts/table_examples/t-14-6.04.R
+++ scripts/table_examples/t-14-6.04.R
@@ -24,6 +24,7 @@ comb <- rbind(adlbc, adlbh)
 comb$LBTEST <-ordered(comb$LBTEST, c(sort(unique(adlbc$LBTEST)), sort(unique(adlbh$LBTEST))))
 comb$TRTP <- ordered(comb$TRTP, c("Placebo", "Xanomeline Low Dose", "Xanomeline High Dose"))
 comb$BLTRFL <- ordered(comb$BLTRFL, c("N", "H"))
+comb$LBTRFL <- ordered(comb$LBTRFL, c("N", "H"))
 comb$VISIT <- ordered(comb$VISIT, c(
   "WEEK 2",
   "WEEK 4",
@@ -37,65 +38,10 @@ comb$VISIT <- ordered(comb$VISIT, c(
 ))
 
 comb2 <- comb %>%
-  filter(!is.na(VISIT))
+  filter(!is.na(VISIT), !is.na(TRTP), !is.na(BLTRFL), !is.na(LBTRFL)) %>%
+  group_by(LBTEST, VISIT, TRTP, BLTRFL, LBTRFL) %>%
+  complete(nesting(BLTRFL, LBTRFL)) %>%
+  summarise(N = n()) %>%
+  pivot_wider(id_cols = c(LBTEST, VISIT, LBTRFL), names_from = c(TRTP, BLTRFL), values_from = N)
 
-comb3 <- ddply(comb2, "LBTEST", function(test_i){
-
-  ddply(df1, "VISIT", function(visit_i) {
-
-    #returned as a list so they can be cbinded
-    df3 <- dlply(df2, "TRTP", function(trtp_i) {
-      ns <- c(
-        nrow(comb2[comb2$LBTEST == test_i &
-                   comb2$VISIT == visit_i &
-                   comb2$TRTP == trtp_i   &
-                   comb2$BLTRFL == "N", ]),
-        nrow(comb2[comb2$LBTEST == test_i &
-                     comb2$VISIT == visit_i &
-                     comb2$TRTP == trtp_i   &
-                     comb2$BLTRFL == "H", ])
-      )
-      norms <- c(
-        nrow(comb2[comb2$LBTEST == test_i   &
-                     comb2$VISIT == visit_i &
-                     comb2$TRTP == trtp_i   &
-                     comb2$BLTRFL == "N"    &
-                     comb2$LBTRFL == "N", ]),
-        nrow(comb2[comb2$LBTEST == test_i   &
-                     comb2$VISIT == visit_i &
-                     comb2$TRTP == trtp_i   &
-                     comb2$BLTRFL == "N"    &
-                     comb2$LBTRFL == "H", ])
-      )
-      highs <- c(
-        nrow(comb2[comb2$LBTEST == test_i   &
-                     comb2$VISIT == visit_i &
-                     comb2$TRTP == trtp_i   &
-                     comb2$BLTRFL == "H"    &
-                     comb2$LBTRFL == "N", ]),
-        nrow(comb2[comb2$LBTEST == test_i   &
-                     comb2$VISIT == visit_i &
-                     comb2$TRTP == trtp_i   &
-                     comb2$BLTRFL == "H"    &
-                     comb2$LBTRFL == "H", ])
-      )
-
-      data.frame(
-        `Normal at Baseline` = c(
-          ns[1],
-          norms[1],
-          highs[1]
-        ),
-        `High at Baseline` = c(
-          ns[2],
-          norms[2],
-          highs[2]
-        ),
-        check.names = FALSE
-      )
-    })
-
-
-  })
-})
 
