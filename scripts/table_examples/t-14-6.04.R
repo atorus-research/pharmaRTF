763c84fdcf1d7c41c5b7bbed30c24623b840ac4c
Eli Miller <emiller@AR-RStudioDev.4ygzttwrp44enh1pozggg5gc4g.cx.internal.cloudapp.net>
2020-04-21 18:08:41 +0000

    update 6.02

--- scripts/table_examples/t-14-6.04.R
+++ scripts/table_examples/t-14-6.04.R
@@ -27,6 +27,21 @@ pad_row <- function(df, r) {
   df
 }
 
+n_pct <- function(n, pct, n_width=3, pct_width=3) {
+  n <- unlist(n)
+  pct <- unique(pct)
+  # n (%) formatted string. e.g. 50 ( 75%)
+  unlist(lapply(n, function(x) {
+    if(x == 0) " 0      "
+    else {
+      as.character(
+        # Form the string using glue and format
+        glue('{format(x, width=n_width)}({format(round((x/pct) * 100), width=pct_width)}%)')
+      )
+    }
+  }))
+}
+
 ## Chem
 adlbc <- read_xpt(glue("{adam_lib}/adlbc.xpt")) %>%
   filter(SAFETY == "Y", BLTRFL != "")
@@ -72,11 +87,11 @@ comb3 <- comb2 %>%
   rbind(total_bltrfl) %>%
   arrange(LBTEST, VISIT, LBTRFL)
 
-comb3$VISIT <- str_extract(comb3$VISIT, "[0-9]")
-comb3$LBTRFL <- recode(comb3$LBTRFL,
+comb3$VISIT <- as.character(str_extract(comb3$VISIT, "[0-9]+"))
+comb3$LBTRFL <- as.character(recode(comb3$LBTRFL,
                        "T" = "n",
                        "N" = "Normal",
-                       "H" = "High")
+                       "H" = "High"))
 names(comb3) <- c(
   "",
   "Week",
@@ -89,8 +104,89 @@ names(comb3) <- c(
   "High at Baseline"
 )
 
+comb3 <- comb3[!apply(comb3, 1, function(x) {
+  all(as.numeric(x[4:9]) == 0) & all(x[3] == "High")
+}), ]
+
+comb4 <- pad_row(comb3, which(comb3$`Shift to` == "n")) %>%
+  add_row("Week" = NA, .before = 1) %>%
+  add_row("Week" = NA, .before = 1)
+comb4 <- comb4 %>%
+  add_row("Week" = NA, .before = 577) %>%
+  add_row("Week" = NA, .before = 577)
+
+comb4[,1] <- as.character(comb4$...1)
+
+comb4[!(comb4$`Shift to` %in% "n") , 2] <- NA
+comb4[!(comb4$Week %in% "2"), 1] <- NA
+comb4[2,1] <- "CHEMISTRY"
+comb4[3,1] <- "----------"
+comb4[578,1] <- "HEMATOLOGY"
+comb4[579,1] <- "----------"
+
+comb4[!is.na(comb4[,4]), 4]
+
+
+names(comb4) <- c(
+  "",
+  "Week",
+  "Shift to",
+  "Normal at Baseline",
+  "High at Baseline",
+  "Normal at Baseline",
+  "High at Baseline",
+  "Normal at Baseline",
+  "High at Baseline"
+)
+
+dm <- read_xpt(glue("{sdtm_lib}/dm.xpt"))
+headers <- dm %>%
+  filter(ARM != "Screen Failure") %>%
+  group_by(ARM) %>%
+  summarise(N = n()) %>%
+  mutate(label = paste0(recode(ARM,
+                               "Placebo" = "Placebo",
+                               "Xanomeline Low Dose" = "Xan. Low",
+                               "Xanomeline High Dose" = "Xan. High"), " (N=", N, ")"))
 
 
 
+ht <- comb4 %>%
+  as_huxtable(add_colnames = TRUE)
+
+ht <- pad_row(ht, c(1,1))
+ht[1, 4] <- headers[1, "label"]
+ht[1, 6] <- headers[2, "label"]
+ht[1, 8] <- headers[3, "label"]
+
+ht2 <- ht %>%
+  huxtable::merge_cells(1, 4:5) %>%
+  huxtable::merge_cells(1, 6:7) %>%
+  huxtable::merge_cells(1, 8:9) %>%
+  huxtable::set_bottom_border(2, 4:5, 1) %>%
+  huxtable::set_bottom_border(2, 6:7, 1) %>%
+  huxtable::set_bottom_border(2, 8:9, 1) %>%
+  huxtable::set_bottom_border(3, 1:9, 1) %>%
+  huxtable::set_width(1.4) %>%
+  huxtable::set_escape_contents(FALSE) %>%
+  huxtable::set_bold(1:3, 1:9, TRUE) %>%
+  huxtable::set_valign(1:3, 1:9, "bottom") %>%
+  huxtable::set_align(3, 1:9, "center") %>%
+  huxtable::set_col_width(1:9, c(0.29, 0.06, 0.07, rep(0.1, 6)))
+
+
+
+# Write into doc object and pull titles/footnotes from excel file
+doc <- rtf_doc(ht2, header_rows = 3) %>% titles_and_footnotes_from_df(
+  from.file='./scripts/table_examples/titles.xlsx',
+  reader=example_custom_reader,
+  table_number='14-6.04') %>%
+  set_font_size(10) %>%
+  set_ignore_cell_padding(TRUE) %>%
+  set_column_header_buffer(top = 1)
+
+
+# Write out the RTF
+write_rtf(doc, file='./scripts/table_examples/outputs/14-6.04.rtf')
 
 
