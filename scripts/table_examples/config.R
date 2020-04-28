262599a98fd52432cd0c25cd6027ef16ccd74d67
Eli Miller <emiller@AR-RStudioDev.4ygzttwrp44enh1pozggg5gc4g.cx.internal.cloudapp.net>
2020-04-28 16:52:53 +0000

    update 14-7.02

--- scripts/table_examples/config.R
+++ scripts/table_examples/config.R
@@ -4,5 +4,5 @@
 
 # Set data library paths ----
 sdtm_lib <- "../Explore/CDISC_Pilot/m5/datasets/CDISCPILOT01/tabulations"
-adam_lib <- "../Explore/CDISC_Pilot/m5/datasets/CDISCPILOT01/analysis"
-
+old_adam_lib <- "../Explore/CDISC_Pilot/m5/datasets/CDISCPILOT01/analysis"
+adam_lib <- "~/Explore/Updated Pilot Submission Package/900172/m5/datasets/cdiscpilot01/analysis/adam/datasets"
